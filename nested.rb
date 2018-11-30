9ca2adaaba7412e298a616855024ec26e0816e53
jonathan choi <jisung594@gmail.com>
2018-11-30 18:10:42 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -1,4 +1,3 @@
-
 def hopper
 	programmer_hash = 
  		{
@@ -16,6 +15,7 @@ def hopper
         }
      }
 
+grace_hopper = programmer_hash[:grace_hopper]
 
 end
 
@@ -37,6 +37,8 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+  
+  programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +57,8 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     
+  dennis_ritchie = programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
@@ -80,7 +84,9 @@ def adding_matz
         }
      }
 
-    
+  programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]}
+  
+  programmer_hash
 end
 
 def changing_alan
@@ -101,8 +107,10 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
-     
-     
+  
+  programmer_hash[:alan_kay][:known_for] = "GUI"
+  
+  programmer_hash
 end
 
 def adding_to_dennis
@@ -122,4 +130,8 @@ def adding_to_dennis
         }
      }
 
+programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+
+programmer_hash
 end
+
